b89931309a0964e875580b34d3252e0e4793d293
Sathish Vivekanantham <sathish25071992@gmail.com>
2026-01-24 06:02:37 +0000

    Added linux build as part of the build all script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 source "$(dirname "$0")/common.sh"
 
 bash scripts/build_qemu.sh
+bash scripts/build_linux.sh
 bash scripts/build_uboot.sh
 bash scripts/build_atf.sh
 bash scripts/gen_flash.sh
